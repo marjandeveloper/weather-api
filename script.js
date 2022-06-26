0f69d014c6333e8f63e7933eb628c6e183c5d32d
anjaaleksa <marjan1505@gmail.com>
2022-06-26 22:44:22 +0200

    got coord

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const weather = document.getElementById("weather")
-if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getPos);
+const weather = document.getElementById('weather')
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(getPos)
 }
 
 function getPos(position) {
-    const {latitude, longitude} = position.coords;
-    weather.innerHTML = `Latitude: ${latitude} </br> Longitude: ${longitude}`;
+  const { latitude, longitude } = position.coords
+  const cityName = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=640ac0c6c2875d638b61f49ab179ab19`
+  fetch(cityName)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+  //   weather.innerHTML = `Latitude: ${latitude} </br> Longitude: ${longitude}`
 }
